d8c9ff2eed98e104cd6a51e3954b044a5e912d01
Szkiler <sowala@tlen.pl>
2014-12-16 23:36:51 +0100

    Initial Commit

---
+++ Multi-Currency Money/src/org/Dollar.java
@@ -0,0 +1,15 @@
+package org;
+
+public class Dollar {
+
+	int amount;
+
+	public Dollar(int amount) {
+	this.amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+
+}
---
+++ Multi-Currency Money/tests/org/DollarTest.java
@@ -0,0 +1,17 @@
+package org;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DollarTest {
+	
+	@Test
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		Dollar product= five.times(2);
+		assertEquals(10, product.amount);
+		product= five.times(3);
+		assertEquals(15, product.amount);
+		}
+}
